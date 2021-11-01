8e822acc4b067fd6ceaa9a51ca652f7c839f346d
Rodrigo Scalon <rodrigoscalon@gmail.com>
2021-11-01 11:49:57 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div>
+    <>
+      <NavBar />
 
-<h1 className="text-primary"> Hello there! </h1>
+      <div className="conteiner">
 
-    </div>
+        <h1 className="text-primary"> Hello there! </h1>
+
+
+        <DataTable />
+
+      </div>
+
+      <Footer />
+    </>
   );
 }
 

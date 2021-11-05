9c3c28e14a99f82f34fa3122a3446331d45c2d59
Rodrigo Scalon <rodrigoscalon@gmail.com>
2021-11-05 18:07:46 -0300

    FrontEnd test 1

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import Footer from "components/Footer";
+
 import NavBar from "components/NavBar"
 import { Link } from "react-router-dom";
 
@@ -10,17 +10,23 @@ const Home = () => {
 
             <div className="container">
                 <div className="jumbotron">
-                    <h1 className="display-4">DSVendas</h1>
-                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <h1 className="text-left display-4">RSVendas</h1>
+                    <p className="text-left lead">Análise o desempenho das suas vendas por diferentes perspectivas.</p>
                     <hr />
-                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
-                <Link className="btn btn-primary btn-lg" to="/dashboard">
-                    Acessar o dashboard
+
+                    <p className="text-left">Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <p></p>
+               
+                <Link className= "btn btn-outline-dark btn-lg btn-block" to = "/dashboard">
+                    Acessar o Dashboard
                 </Link>
+               
+               
+               
                 </div>
             </div>
 
-            <Footer />
+
         </>
 
     );

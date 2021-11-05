81c071a349f722e237f0540f4ad8106fd349cae9
Rodrigo Scalon <rodrigoscalon@gmail.com>
2021-11-05 11:44:22 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,5 @@
 import ImgDev from 'assets/img/ds-dark.svg';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 
@@ -6,7 +7,10 @@ const NavBar = () => {
         <div className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={ImgDev} alt="Vendas" width="120" />
+
+                    <Link to="/">
+                        <img src={ImgDev} alt="Vendas" width="120" />
+                    </Link>
                 </nav>
             </div>
         </div>
